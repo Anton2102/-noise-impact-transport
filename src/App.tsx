34937a2c29a30fa53c7f7e670e6ac35bc601173b
aa.leshonkov <aa.leshonkov@tensor.ru>
2022-10-24 16:26:36 +0300

    add form option

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Header from './DescriptionBlocks/Header'
-import './App.css';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/app.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    const _choseOption = () => {
+        // 123
+    }
+
+    return (
+        <div className="nit">
+            <Header/>
+            <div className="nit-App__content">
+                <div className="nit-App__content-option">
+                    <Form.Control type="password" placeholder="Введите вариант" />
+                    <Button className="nit-App__content-option-button" variant="primary" type="submit" onClick={_choseOption}>Начать</Button>
+                </div>
+                <div>
+                    <h3>1)</h3>
+                    <div>L<span>ЭКВ</span> + Δ</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
